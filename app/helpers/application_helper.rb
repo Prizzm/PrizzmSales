fb3a1ad6642a36f665fbfe32e24da45ce4432f13
Charles Oppenheimer <choppen5@yahoo.com>
2012-01-17 11:26:01 -0800

    added google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    
+
+    def google_analytics_js
+        
+        content_tag(:script, :type => 'type/javascript') do
+            "var _gaq || [];
+            _gaq.push(['_setAccount', 'UA-28400285-1']);
+            _gaq.push(['_setDomainName', 'prizzm.com']);
+            _gaq.push(['_trackPageview']);
+            
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'type/javascript'; ga.async = true;
+            ga.src = document.getElementByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();"
+        end if Rails.env.production?
+    end
 end
