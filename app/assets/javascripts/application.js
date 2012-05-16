0bae8610cff9845f607f4a95c64ebe614bdee6e0
Giang NGUYEN <ndgiang84@gmail.com>
2012-05-16 14:06:21 +0700

    Fixed signup button in footer.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,11 @@
 //= require jquery.smooth-scroll.min
 //= require modernizr-2.5.3.min
 //= require script
+//= require_self
+
+$(document).ready(function() {
+  $('#footer-signup').bind('click', function(e) {
+    e.preventDefault();
+    $(this).closest('form').submit();
+  });
+})
